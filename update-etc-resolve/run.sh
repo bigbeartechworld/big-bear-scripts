72d5a3d9aa329c2582d680532671e08f7651951d
Christopher <github.bigbeartechworld@emailcove.com>
2023-09-15 19:00:12 -0500

    Show current DNS server

--- update-etc-resolve/run.sh
+++ update-etc-resolve/run.sh
@@ -3,8 +3,12 @@
 # Define the path to the resolv.conf file
 resolv_conf="/etc/resolv.conf"
 
+# Display the current nameserver entry
+current_dns_server=$(grep '^nameserver' "$resolv_conf" | awk '{print $2}')
+echo "Current DNS server: $current_dns_server"
+
 # Prompt the user for DNS server input or use default
-read -p "Enter DNS server IP (default is 1.1.1.1): " dns_server
+read -p "Enter new DNS server IP (default is 1.1.1.1): " dns_server
 dns_server=${dns_server:-1.1.1.1}
 
 # Backup the original resolv.conf file
