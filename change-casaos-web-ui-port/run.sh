7232d3202feb90204953e995ac185de5e6a62613
Christopher <github.bigbeartechworld@emailcove.com>
2023-09-23 21:15:32 -0500

    Add change casaos web ui port

---
+++ change-casaos-web-ui-port/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Ask the user for the new port number
+read -p "Enter the new port number for CasaOS: " new_port
+
+# Check if the input is a valid number
+if ! [[ "$new_port" =~ ^[0-9]+$ ]]; then
+    echo "Error: Please enter a valid port number."
+    exit 1
+fi
+
+# Backup the original configuration file
+cp /etc/casaos/gateway.conf /etc/casaos/gateway.conf.bak
+
+# Change the port number in the configuration file
+sed -i "s/^port=[0-9]\+/port=$new_port/" /etc/casaos/gateway.conf
+
+# Restart the CasaOS Gateway service
+systemctl restart casaos-gateway.service
+
+echo "Port changed and CasaOS Gateway service restarted successfully."
