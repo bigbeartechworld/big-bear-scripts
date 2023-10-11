d1af2c7cac63eaadb54141c369f74f84eb85936a
Christopher <github.bigbeartechworld@emailcove.com>
2023-10-11 18:07:58 -0500

    Add docker-exec-helper script

---
+++ docker-exec-helper/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# List all running containers
+containers=$(docker ps --format "{{.Names}}")
+IFS=$'\n' read -rd '' -a containerArray <<< "$containers"
+
+echo "Select a Docker container to exec into:"
+counter=1
+for container in "${containerArray[@]}"; do
+    echo "$counter. $container"
+    let counter++
+done
+
+# Get user's choice
+read -p "Enter the number of the container: " choice
+selectedContainer="${containerArray[$choice-1]}"
+
+# Check if user wants additional arguments
+read -p "Do you want to provide additional arguments? (e.g., -u www-data) [y/N]: " additionalArgsChoice
+additionalArgs=""
+if [[ $additionalArgsChoice == "y" || $additionalArgsChoice == "Y" ]]; then
+    read -p "Enter the additional arguments: " additionalArgs
+fi
+
+# Docker exec into the chosen container
+docker exec -it $additionalArgs $selectedContainer /bin/bash
