53225054d4e2d7df65b6d84fd71d41d21f417fa5
Christopher <github.bigbeartechworld@emailcove.com>
2023-11-01 14:56:53 -0500

    Update docker compose to v2.23.0

--- install-docker/run.sh
+++ install-docker/run.sh
@@ -62,7 +62,7 @@ sudo docker run hello-world
 # Ask if the user wants to install Docker Compose
 if confirm "Do you want to install Docker Compose?"; then
     echo "Installing Docker Compose..."
-    sudo curl -L "https://github.com/docker/compose/releases/download/v2.6.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
     echo "Docker Compose installed successfully."
 else
