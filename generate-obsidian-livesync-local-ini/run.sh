b74f7c8b15add4d5e6b4363fa46bd37562eea38f
Diego <dieqohc@gmail.com>
2023-12-25 18:06:35 -0500

    Update run.sh
    Fixing an issue where running the original script tried to crete local.ini as a directory.

--- generate-obsidian-livesync-local-ini/run.sh
+++ generate-obsidian-livesync-local-ini/run.sh
@@ -5,12 +5,13 @@ read -p "Enter the location to save the config (default: /DATA/AppData/big-bear-
 
 # If the user doesn't provide a location, default to the specified path
 if [ -z "$location" ]; then
-    location="/DATA/AppData/big-bear-obsidian-livesync/data/local.ini"
+    dir_location="/DATA/AppData/big-bear-obsidian-livesync/data"
+    file_path="$dir_location/local.ini"
 fi
 
 # Check if the config file already exists
-if [ -e "$location" ]; then
-    read -p "Warning: $location already exists. Do you want to replace it? (yes/no) " replace
+if [ -e "$file_path" ]; then
+    read -p "Warning: $file_path already exists. Do you want to replace it? (yes/no) " replace
     if [[ "$replace" != "yes" ]]; then
         echo "Operation cancelled."
         exit 1
@@ -18,10 +19,10 @@ if [ -e "$location" ]; then
 fi
 
 # Create the directory (and its parents) if it doesn't exist
-mkdir -p "$(dirname "$location")"
+mkdir -p "$(dirname "$dir_location")"
 
 # Download the file from the given URL and save it to the specified location
-curl -L "https://raw.githubusercontent.com/bigbeartechworld/big-bear-casaos/master/Apps/obsidian-livesync/local.ini" -o "$location"
+curl -L "https://raw.githubusercontent.com/bigbeartechworld/big-bear-casaos/master/Apps/obsidian-livesync/local.ini" -o "$file_path"
 
 # Confirm to the user
-echo "Config saved to $location"
+echo "Config saved to $file_path"
