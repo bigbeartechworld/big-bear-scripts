bfa80c120e3f1a8650449a515ae292b0b6dc874f
Christopher <github.bigbeartechworld@emailcove.com>
2024-03-14 08:05:19 -0500

    Update config file location in run.sh script for Romm

--- generate-romm-config/run.sh
+++ generate-romm-config/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Ask the user for the desired config location
-read -p "Enter the location to save the config (default: /DATA/AppData/big-bear-romm/config.yml): " location
+read -p "Enter the location to save the config (default: /DATA/AppData/big-bear-romm/config/config.yml): " location
 
 # If the user doesn't provide a location, default to the specified path
 if [ -z "$location" ]; then
-    location="/DATA/AppData/big-bear-romm/config.yml"
+    location="/DATA/AppData/big-bear-romm/config/config.yml"
 fi
 
 # Check if the config file already exists
@@ -21,7 +21,7 @@ fi
 mkdir -p "$(dirname "$location")"
 
 # Download the file from the given URL and save it to the specified location
-curl -L "https://raw.githubusercontent.com/bigbeartechworld/big-bear-casaos/master/Apps/romm/config.yml" -o "$location"
+curl -L "https://raw.githubusercontent.com/bigbeartechworld/big-bear-casaos/master/Apps/Romm/config.yml" -o "$location"
 
 # Confirm to the user
 echo "Config saved to $location"
