c7e92057564de20816f880f3a10c0b372fe95ab2
Christopher <github.bigbeartechworld@emailcove.com>
2024-09-26 11:38:10 -0500

    ✨ feat: Bump healthcheck version to 3.0
    Bumped the version of the BigBearCasaOS Healthcheck tool from 2.3 to 3.0.
    This change reflects the latest updates and improvements made to the
    healthcheck script.

--- casaos-healthcheck/run.sh
+++ casaos-healthcheck/run.sh
@@ -303,7 +303,7 @@ elif [[ "$1" == "real_test" ]]; then
 else
     # Normal script execution
     # Display Welcome
-    print_header "BigBearCasaOS Healthcheck V2.3"
+    print_header "BigBearCasaOS Healthcheck V3.0"
     echo "Here are some links:"
     echo "https://community.bigbeartechworld.com"
     echo "https://github.com/BigBearTechWorld"
